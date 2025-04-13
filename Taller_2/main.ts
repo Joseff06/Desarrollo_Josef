c4de69d690df4741d207d4541a8350b5e88e1a1f
Josef Alferez <J.alferez@uniandes.edu.co>
2025-04-13 17:07:39 -0500

    Taller 1 y 2

--- Taller_2/main.ts
+++ Taller_2/main.ts
@@ -1,12 +1,10 @@
-// main.ts
-import { series } from "./data"; 
+import { series, Serie } from "./data";
 
-
-const seriesTbody: HTMLElement = document.getElementById("series")!; 
+const seriesTbody: HTMLElement = document.getElementById("series")!;
 const averageElm: HTMLElement = document.getElementById("seasons-average")!;
+const cardContainer: HTMLElement = document.getElementById("card-container")!;
 
-
-function getAverageSeasons(seriesList: any[]): number {
+function getAverageSeasons(seriesList: Serie[]): number {
   let totalSeasons: number = 0;
   seriesList.forEach((serie) => {
     totalSeasons += serie.seasons;
@@ -14,23 +12,40 @@ function getAverageSeasons(seriesList: any[]): number {
   return totalSeasons / seriesList.length;
 }
 
-function renderSeriesInTable(seriesList: any[]): void {
+function renderSeriesInTable(seriesList: Serie[]): void {
   seriesList.forEach((serie) => {
-
     const trElement = document.createElement("tr");
-
     trElement.innerHTML = `
       <td>${serie.id}</td>
-      <td>${serie.name}</td>
+      <td class="text-primary">${serie.name}</td>
       <td>${serie.channel}</td>
       <td>${serie.seasons}</td>
     `;
 
+    trElement.addEventListener("click", () => {
+      showSerieDetail(serie);
+    });
 
     seriesTbody.appendChild(trElement);
   });
 }
 
+function showSerieDetail(serie: Serie): void {
+  cardContainer.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <img src="${serie.image}" class="card-img-top" alt="${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">
+          ${serie.description}
+        </p>
+        <a href="${serie.link}" class="btn btn-primary" target="_blank">
+          Go to website
+        </a>
+      </div>
+    </div>
+  `;
+}
 
 renderSeriesInTable(series);
-averageElm.innerText = getAverageSeasons(series).toFixed(2); 
+averageElm.textContent = getAverageSeasons(series).toFixed(2);
